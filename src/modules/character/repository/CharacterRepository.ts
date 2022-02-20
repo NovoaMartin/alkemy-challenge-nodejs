253bb535bcddbe5f6b5a961ef56d8d61846041b3
Martin Novoa <martinnovoar2@gmail.com>
2022-02-20 01:51:36 -0300

    Add save and update function in characterRepository

--- src/modules/character/repository/CharacterRepository.ts
+++ src/modules/character/repository/CharacterRepository.ts
@@ -6,46 +6,59 @@ import { fromModelToCharacter, fromModelToCharacterList } from '../mapper/charac
 import CharacterNotFoundException from '../exception/CharacterNotFoundException';
 import Character from '../entity/Character';
 import FilmModel from '../../../models/FilmModel';
+import InvalidFilmGivenException from '../exception/InvalidFilmGivenException';
 
 interface ISearchParams {
   name?: string | null;
   age?: number | null;
   weight?: number | null;
-  filmName? : string | null;
+  filmName?: string | null;
 }
 
-
 export default class CharacterRepository {
-  constructor(private characterModel : typeof CharacterModel) {}
+  constructor (private characterModel: typeof CharacterModel, private filmModel: typeof FilmModel) {}
 
-  async getAll() : Promise<CharacterListDTO[]> {
+  async getAll (): Promise<CharacterListDTO[]> {
     return (await this.characterModel.findAll({ attributes: ['id', 'name', 'image'] })).map(
       (item) => fromModelToCharacterList(item),
     );
   }
 
-  async getById(id : string) : Promise<Character> {
+  async getById (id: string): Promise<Character> {
     const result = await this.characterModel.findByPk(id, { include: FilmModel });
     if (!result) throw new CharacterNotFoundException();
     return fromModelToCharacter(result);
   }
 
-  async save(character : Partial<Character>) {
+  async save (character: Partial<Character>, associatedFilms?: string[]) {
+    let films: FilmModel[] = [];
+    if (associatedFilms) {
+      films = await this.filmModel.findAll({
+        where: {
+          id: { [Op.in]: associatedFilms }
+        }
+      });
+      if (films.length !== associatedFilms.length)
+        throw new InvalidFilmGivenException();
+    }
+
     const isNewRecord = !character.id;
     if (isNewRecord) {
       character.id = v4();
     }
     const instance = this.characterModel.build(character, { isNewRecord });
-    return (await instance.save()).toJSON();
+    await instance.save();
+    await instance.setFilms(films);
+    return fromModelToCharacter(await instance.reload());
   }
 
-  async delete(id : string) {
+  async delete (id: string) {
     return !!(await this.characterModel.destroy({ where: { id } }));
   }
 
-  async search({name, age, weight, filmName} : ISearchParams) {
-    const whereCondition : any = {};
-    let includeCondition : any =[];
+  async search ({ name, age, weight, filmName }: ISearchParams) {
+    const whereCondition: any = {};
+    let includeCondition: any = [];
     if (name) {
       whereCondition.name = { [Op.like]: `%${name}%` };
     }
@@ -55,14 +68,14 @@ export default class CharacterRepository {
     if (weight) {
       whereCondition.weight = weight;
     }
-    if(filmName) {
+    if (filmName) {
       includeCondition = [
-        { model: FilmModel, where: { title: { [Op.like]: `%${filmName}%`}} }
-      ]
+        { model: FilmModel, where: { title: { [Op.like]: `%${filmName}%` } } }
+      ];
     }
-     return(await this.characterModel.findAll({  where:whereCondition, include:includeCondition } ))
+    return (await this.characterModel.findAll({ where: whereCondition, include: includeCondition }))
       .map(
-      (item) => fromModelToCharacterList(item),
-    );
+        (item) => fromModelToCharacterList(item),
+      );
   }
 }
